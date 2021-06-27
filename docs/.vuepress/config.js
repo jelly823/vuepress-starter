82242ab8dfc0aca7366b518599eb14e3944c030e
jelly <jellyl823@163.com>
2021-06-27 13:32:49 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,39 @@
 module.exports = {
     title: 'Hello Jelly!',
-    description: 'More interests for you!'
+    description: 'More interests for you!',
+    base:'/vuepress-starter/',
+    theme: 'cool',
+    markdown: {
+        // markdown-it-anchor 的选项
+        anchor: { permalink: false },
+        // markdown-it-toc 的选项
+        toc: { includeLevel: [1, 2] },
+        extendMarkdown: md => {
+            md.set({ html: true })
+            // 使用更多的 markdown-it 插件!
+            md.use(require('markdown-it-katex'))
+            md.use(require('markdown-it-plantuml'))
+            md.use(require('markdown-it-admonition'))
+        },
+    },
+    themeConfig: {
+        logo: '/logo.svg',
+        lastUpdated: 'Last Updated', // string | boolean
+        mdEnhance: {
+            mermaid: true,
+            lineNumbers: false,
+        },
+        iconPrefix: 'icon-',
+        feed: false,
+        hostname: 'https://jellyl823.github.io/',
+        nav:[
+            {text:'Home',link:'/',icon: 'home'},
+            {text:'HTML',link:'/HTML/'},
+            {text:'Javascript',link:'/Javascript/'}
+        ],
+        sidebar:[
+            '/',
+            {}
+        ],
+    },
 }
--- docs/guide/README.md
+++ docs/guide/README.md
@@ -1,1 +1,47 @@
 ## Guide page
+
+|col1    |col2    |col3   |
+|--------|:------:|------:|
+|row1    |haha    |haha   |
+|row2    |hoho    |hoho   |
+
+:tada: :100:
+
+::: tip 
+首页
+:::
+
+::: danger
+千万不要做
+:::
+
+::: warning
+还是不要做为妙
+:::
+
+::: details 详情展开
+你瞅啥
+:::
+
+``` js {4}
+export default {
+  data () {
+    return {
+      msg: 'Highlighted!'
+    }
+  }
+}
+```
+
+
+<mermaid>
+graph TD
+  A[Cool] -->|Get money| B(Go shopping)
+  B --> C{Let me}
+  C -->|Two| D[Laptop]
+  C -->|Two| E[iPhone]
+  C -->|Three| F[Car]
+  C -->|Four| F[Mac]
+</mermaid>
+
+<sample-timeline />
