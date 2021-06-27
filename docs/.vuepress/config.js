c7362e34d5006e548261770b26c490be56c3e484
jelly <jellyl823@163.com>
2021-06-27 17:10:22 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,5 @@
 //const sidebar = require('./sidebar')
+const nav = require('./nav')
 module.exports = {
     title: 'blog-jelly',
     description: 'More interests for you!',
@@ -27,16 +28,11 @@ module.exports = {
         iconPrefix: 'icon-',
         feed: false,
         hostname: 'https://jellyl823.github.io/',
-        nav: [
-            { text: 'Home', link: '/', icon: 'home' },
-            { text: 'HTML', link: '/HTML/' },
-            { text: 'CSS', link: '/CSS/' },
-            { text: 'Javascript', link: '/Javascript/' }
-        ],
+        nav,
         sidebar: [
             {
                 title: 'HTML',
-                path:'/HTML/',
+                path: '/HTML/',
                 collapsable: true,
                 children: [
                     {
@@ -51,4 +47,32 @@ module.exports = {
             },
         ]
     },
+    plugins: [
+        [
+            'vuepress-plugin-helper-live2d', {
+                // 是否开启控制台日志打印(default: false)
+                log: false,
+                live2d: {
+                    // 是否启用(关闭请设置为false)(default: true)
+                    enable: true,
+                    // 模型名称(default: hibiki)>>>取值请参考：
+                    // https://github.com/JoeyBling/hexo-theme-yilia-plus/wiki/live2d%E6%A8%A1%E5%9E%8B%E5%8C%85%E5%B1%95%E7%A4%BA
+                    model: 'hijiki',// 黑色背景下换成这个'tororo'
+                    display: {
+                        position: "right", // 显示位置：left/right(default: 'right')
+                        width: 135, // 模型的长度(default: 135)
+                        height: 300, // 模型的高度(default: 300)
+                        hOffset: 65, //  水平偏移(default: 65)
+                        vOffset: 0, //  垂直偏移(default: 0)
+                    },
+                    mobile: {
+                        show: false // 是否在移动设备上显示(default: false)
+                    },
+                    react: {
+                        opacity: 0.8 // 模型透明度(default: 0.8)
+                    }
+                }
+            }
+        ]
+    ]
 }
---
+++ docs/.vuepress/nav.js
@@ -0,0 +1,16 @@
+module.exports = [
+    { text: 'Home', link: '/', icon: 'home' },
+    { text: 'guide', link: '/guide/' },
+    { text: 'HTML', items: [
+        {
+            text:'HTML5语义化标签',
+            link:'/HTML/HTML5语义化标签'
+        },
+        {
+            text:'响应式设计',
+            link:'/HTML/响应式设计'
+        },
+    ] },
+    { text: 'CSS', link: '/CSS/' },
+    { text: 'Javascript', link: '/Javascript/' }
+]
