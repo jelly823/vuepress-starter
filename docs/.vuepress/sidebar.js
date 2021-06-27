b3065826c2744c36937d951615ce2c67552914a9
LastKnightCoder <2223106858@qq.com>
2021-06-27 18:07:23 +0800

    Update sidebar.js

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,5 +1,17 @@
 module.exports = {
-    "HTML": {
-        
-    }
+    "HTML": [{
+        title: '基础',
+        collapsable: false,
+        children: [{
+            title: 'HTML5语义化标签',
+            link: '/HTML/HTML5语义化标签'
+        }]
+    }, {
+        title: '高级',
+        collapsable: false,
+        children: [{
+            title: '响应式设计',
+            link: '/HTML/响应式设计'
+        }]
+    }]
 }
